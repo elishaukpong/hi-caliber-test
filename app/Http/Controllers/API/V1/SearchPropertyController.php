6c631272a631537b32df65ff9c92c1105384ac2c
Elisha Ukpong <ishukpong418@gmail.com>
2024-06-27 11:07:54 +0100

    added property resource class

--- app/Http/Controllers/API/V1/SearchPropertyController.php
+++ app/Http/Controllers/API/V1/SearchPropertyController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\API\V1\PropertyResource;
 use App\Models\Property;
 
 class SearchPropertyController extends Controller
 {
     public function __invoke()
     {
-        return Property::all();
-
+        return PropertyResource::collection(Property::paginate());
     }
 }
