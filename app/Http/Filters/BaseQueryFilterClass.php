2e12aae87dd09cc832dc3d11aa6941340f8f0a8b
Elisha Ukpong <ishukpong418@gmail.com>
2024-06-27 11:17:50 +0100

    aded filtering logic

--- app/Http/Filters/BaseQueryFilterClass.php
+++ app/Http/Filters/BaseQueryFilterClass.php
@@ -2,7 +2,38 @@
 
 namespace App\Http\Filters;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
 abstract class BaseQueryFilterClass
 {
+    protected Builder $builder;
 
+    public function __construct(protected Request $request)
+    {
+        //
+    }
+
+    public function filter(array $array): void
+    {
+        $this->iterateAndCallMethod($array);
+    }
+
+    public function apply(Builder $builder): Builder
+    {
+        $this->builder = $builder;
+
+        $this->iterateAndCallMethod($this->request->all());
+
+        return $builder;
+    }
+
+    public function iterateAndCallMethod(array $array): void
+    {
+        foreach ($array as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+    }
 }
--- app/Http/Filters/PropertyFilterClass.php
+++ app/Http/Filters/PropertyFilterClass.php
@@ -2,7 +2,43 @@
 
 namespace App\Http\Filters;
 
+use Illuminate\Database\Eloquent\Builder;
+
 class PropertyFilterClass extends BaseQueryFilterClass
 {
+    public function name($value): Builder
+    {
+        return $this->builder->where('name', 'LIKE', $value . '%');
+    }
 
+    public function bedrooms($value): Builder
+    {
+        return $this->builder->where('bedroom_count', $value);
+    }
+
+    public function bathrooms($value): Builder
+    {
+        return $this->builder->where('bathroom_count', $value);
+    }
+
+    public function storeys($value): Builder
+    {
+        return $this->builder->where('storey_count', $value);
+    }
+
+    public function garages($value): Builder
+    {
+        return $this->builder->where('garage_count', $value);
+    }
+
+    public function price($value): Builder
+    {
+        $createdAt = explode(',', $value);
+
+        if (count($createdAt) > 1) {
+            return $this->builder->whereBetween('price', $createdAt);
+        }
+
+        return $this->builder->whereDate('price', $createdAt);
+    }
 }
