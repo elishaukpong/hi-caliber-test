ee8bf7dfdf84894ebb17401192dae8cebd864e74
Elisha Ukpong <ishukpong418@gmail.com>
2024-06-27 11:10:42 +0100

    specified response json data

--- app/Http/Resources/API/V1/PropertyResource.php
+++ app/Http/Resources/API/V1/PropertyResource.php
@@ -14,6 +14,19 @@ class PropertyResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'type' => 'property',
+            'id' => $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'price' => $this->price,
+                'bedrooms' => $this->bedroom_count,
+                'bathrooms' => $this->bathroom_count,
+                'storeys' => $this->storey_count,
+                'garages' => $this->garage_count,
+                'createdAt' => $this->created_at->format('d-m-Y h:i:s'),
+                'updatedAt' => $this->updated_at->format('d-m-Y h:i:s')
+            ]
+        ];
     }
 }
