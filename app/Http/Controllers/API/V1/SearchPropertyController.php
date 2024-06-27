4efe8ec44c9b6e71db98bb9101f9c942046bbb1e
Elisha Ukpong <ishukpong418@gmail.com>
2024-06-27 10:59:54 +0100

    updated env example

--- app/Http/Controllers/API/V1/SearchPropertyController.php
+++ app/Http/Controllers/API/V1/SearchPropertyController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Property;
 
 class SearchPropertyController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        return Property::all();
+
+    }
 }
