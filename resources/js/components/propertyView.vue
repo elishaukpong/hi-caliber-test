91be679b9e2b8edaa4aac7dd1efd5faeb715085e
Elisha Ukpong <ishukpong418@gmail.com>
2024-06-27 11:51:18 +0100

    added lifecycle hook to populate data

--- resources/js/components/propertyView.vue
+++ resources/js/components/propertyView.vue
@@ -7,12 +7,26 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 import PropertySearch from "./propertySearchView.vue";
 import PropertyListing from "./propertyListingView.vue";
 
 let properties = ref([]);
+
+async function getPropertyData() {
+  try {
+    await axios.get('http://localhost:81/api/v1/properties')
+        .then(response => response.data)
+        .then(response => properties.value = response.data);
+  } catch (error) {
+    console.error('Error fetching property data:', error);
+  }
+}
+
+onMounted(() => {
+  getPropertyData();
+});
 
 
 </script>
