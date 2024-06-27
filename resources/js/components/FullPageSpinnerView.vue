376365a8473678a8d855bbe6fb5fe00514663e30
Elisha Ukpong <ishukpong418@gmail.com>
2024-06-27 12:19:14 +0100

    added loading state

---
+++ resources/js/components/FullPageSpinnerView.vue
@@ -0,0 +1,19 @@
+<!-- FullPageSpinner.vue -->
+<template>
+  <div v-if="show" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+    <div class="spinner-border animate-spin inline-block w-12 h-12 border-4 border-white border-t-transparent rounded-full" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  show: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
--- resources/js/components/propertyView.vue
+++ resources/js/components/propertyView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container mx-auto mt-10">
+    <FullPageSpinner :show="show"></FullPageSpinner>
     <PropertySearch @searchProperty="searchProperty" @cancelSearchProperty="cancelSearch"></PropertySearch>
 
     <PropertyListing :properties="properties"></PropertyListing>
@@ -11,8 +12,10 @@ import { ref, onMounted } from 'vue';
 
 import PropertySearch from "./propertySearchView.vue";
 import PropertyListing from "./propertyListingView.vue";
+import FullPageSpinner from "./FullPageSpinnerView.vue";
 
 let properties = ref([]);
+let show = ref(false);
 
 onMounted(() => {
   makeNetworkRequestForProperties();
@@ -26,6 +29,10 @@ function cancelSearch(event) {
   makeNetworkRequestForProperties();
 }
 
+function toggleSpinner() {
+  show.value = !show.value;
+}
+
 async function makeNetworkRequestForProperties(params = '') {
   let url = 'http://localhost:81/api/v1/properties';
 
@@ -34,10 +41,16 @@ async function makeNetworkRequestForProperties(params = '') {
   }
 
   try {
+    toggleSpinner();
+
     await axios.get(url)
         .then(response => response.data)
         .then(response => properties.value = response.data);
+
+    toggleSpinner();
   } catch (error) {
+    toggleSpinner();
+
     properties.value = [];
 
     console.error('Error fetching property data:', error);
